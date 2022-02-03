33a0593fd0b17e8bccd88ebd6a8ad9b471dfbe4f
Menake Dasunpriya <menakedasunpriya@gmail.com>
2022-02-03 19:49:32 +0530

    add: favicon

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link rel='shortcut icon' href='/favicon.png' type='image/x-icon' />
+        </Head>
         <body>
           <Main />
           <NextScript />
